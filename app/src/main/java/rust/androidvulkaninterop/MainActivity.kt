4cb7e93476255c1ce6c8c76c8d7377a05fd7faba
Marijn Suijten <marijns95@gmail.com>
2023-10-24 22:58:18 +0200

    Initial stripped-down Gradle (Kotlin DSL) + Kotlin app

---
+++ app/src/main/java/rust/androidvulkaninterop/MainActivity.kt
@@ -0,0 +1,19 @@
+package rust.androidvulkaninterop
+
+import android.app.Activity
+import android.graphics.SurfaceTexture
+import android.os.Bundle
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.TextureView
+
+class MainActivity : Activity() {
+    companion object {
+        init {}
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
